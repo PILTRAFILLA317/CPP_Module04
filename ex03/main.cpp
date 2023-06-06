31726fd0a94ebfa88668acf635148b6bed71cccd
Unai Martin marante <umartin-@c2r3s6.42urduliz.com>
2023-06-06 13:23:33 +0200

    more ex3 tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -5,7 +5,7 @@
 #include "MateriaSource.hpp"
 #include "Character.hpp"
 
-int main()
+void easyTest()
 {
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
@@ -22,6 +22,40 @@ int main()
 	delete bob;
 	delete me;
 	delete src;
-	delete tmp;
+}
+
+void hardTest(){
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+	me->use(4, *bob);
+	me->use(-2, *bob);
+	me->use(-1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+}
+
+int main(){
+	easyTest();
+	std::cout << std::endl;
+	hardTest();
 	return 0;
 }
